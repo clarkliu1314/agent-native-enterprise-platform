2abc9bade8b08db370df8b9ebb2dab8ead9e061f
Clark Liu <gogogirl163@163.com>
2026-09-12 01:05:42 +0800

    feat(runtime): export durable runtime services

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,1 +1,6 @@
 export { InMemoryAgentRuntime } from './in-memory-runtime';
+export { DurableRuntimeService, hashCommand } from './runtime-service';
+export { PostgresRuntimeRepositories } from './postgres-runtime-repositories';
+export type * from './ports';
+export type * from './repositories';
+export * from './errors';
