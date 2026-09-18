e77191b78497ad79f9b6be1429943e7d4675acad
Clark Liu <gogogirl163@163.com>
2026-09-18 22:11:48 +0800

    refactor: point benchmark runner to platform executor

--- packages/benchmark/src/benchmark-runner.ts
+++ packages/benchmark/src/benchmark-runner.ts
@@ -1,5 +1,5 @@
 import { platformBenchmarkCases } from './cases/platform';
 import { createBenchmarkRunner } from './framework/scenario-runner';
-import { executeBenchmarkScenario } from './scenario-executor';
+import { executeBenchmarkScenario } from './cases/platform/scenario-executor';
 
 export const benchmarkRunner = createBenchmarkRunner(platformBenchmarkCases, executeBenchmarkScenario);
